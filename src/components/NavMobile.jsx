bf38d7749cf0f101a36632ddfcab87d04e704ede
KvinPardo <kevin.pardov25@gmail.com>
2023-07-20 11:21:57 -0400

    .

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -18,38 +18,22 @@ const NavMobile = ({ navMobile }) => {
           </Link>
         </li>
         <li>
-          <Link
-            to="nosotros"
-            onClick={handleClick}
-            className="text-primary text-2xl"
-          >
+          <Link to="/nosotros" className="text-primary text-2xl">
             Nosotros
           </Link>
         </li>
         <li>
-          <Link
-            to="nuestro-equipo"
-            onClick={handleClick}
-            className="text-primary text-2xl"
-          >
+          <Link to="/nuestro-equipo" className="text-primary text-2xl">
             Nuestro Equipo
           </Link>
         </li>
         <li>
-          <Link
-            to="productos"
-            onClick={handleClick}
-            className="text-primary text-2xl"
-          >
+          <Link to="/productos" className="text-primary text-2xl">
             Productos
           </Link>
         </li>
         <li>
-          <Link
-            to="contacto"
-            onClick={handleClick}
-            className="text-primary text-2xl"
-          >
+          <Link to="/contacto" className="text-primary text-2xl">
             Contacto
           </Link>
         </li>
